14748cddf30346e206231855febf0e068eea3663
yangr <yangr@t3go.cn>
2021-04-14 15:06:41 +0800

    update .gitignore

--- config/config.default.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-module.exports = appInfo => ({
-  keys: 'coco', // 用于 cookie 的加解密，上线后就不要改了。
-  logger: {
-    dir: path.join(__dirname, '../logs/coco-server'),
-  }
-});
